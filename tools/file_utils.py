f33d15b279c3e112aa76a530d9b8619dcb301b62
Iurii Nartov <yurahttp@gmail.com>
2026-03-11 18:34:59 +0200

    Update file_utils.py

--- tools/file_utils.py
+++ tools/file_utils.py
@@ -15,11 +15,11 @@ class FileUtils:
     log = Logger(__name__)
 
     @classmethod
-    def timestamped_path(cls, file_name, file_ext, path_to_file=os.getenv("HOST_ARTIFACTS")):
+    def timestamped_path(cls, file_name, file_ext, path_to_file=os.getenv("HOST_ARTIFACTS")) -> str:
         """
         Args:
             file_name (str): e.g. screenshot
-            file_ext (str): file extention, e.g., png
+            file_ext (str): file extension, e.g., png
             path_to_file (str): e.g. /home/user/test_dir/artifacts/
         """
         ts = datetime.utcnow().strftime("%Y%m%d-%H%M%S.%f")
@@ -30,6 +30,6 @@ class FileUtils:
     @staticmethod
     def ensure_dir(path: str) -> None:
         """
-        Creating directories forcely
+        Creating directories forcefully
         """
         os.makedirs(path, exist_ok=True)
